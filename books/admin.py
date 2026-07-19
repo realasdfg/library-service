5b1f810b543d7fdd33bab571ebb52a3d017df932
Roman Popadiuk <realasdfga@gmail.com>
2026-07-19 19:45:46 +0200

    Register Book model in admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "inventory", "daily_fee")
+    list_filter = ("author",)
+    search_fields = ("title",)
