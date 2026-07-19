35d3ea468a232a50f3e51814dce219c599792b8f
Roman Popadiuk <realasdfga@gmail.com>
2026-07-20 00:55:47 +0200

    Implement user managing endpoint me/

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from users.serializers import UserSerializer
 
@@ -6,3 +7,11 @@ from users.serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = ()
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
