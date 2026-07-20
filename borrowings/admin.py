189ef09d6e66d438c4f7204d91800eec243a602c
Roman Popadiuk <realasdfga@gmail.com>
2026-07-20 20:57:09 +0200

    Register Borrowing model in admin

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from borrowings.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "book__title",
+        "user__email",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+    )
+    list_filter = ("book__title", "user__email", "borrow_date", "expected_return_date")
+    search_fields = ("book__title",)
