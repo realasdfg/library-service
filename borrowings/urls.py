a49625f83aab2a4b15d6896fbec84afc2e00c837
Roman Popadiuk <realasdfga@gmail.com>
2026-07-21 01:46:47 +0200

    Implement Borrowing creation endpoint

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from borrowings.views import BorrowingReadViewSet
+from borrowings.views import BorrowingViewSet
 
 router = routers.DefaultRouter()
-router.register("borrowings", BorrowingReadViewSet)
+router.register("borrowings", BorrowingViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
