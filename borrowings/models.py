ea3026a09d5f81572471e4d901179fba20183382
Roman Popadiuk <realasdfga@gmail.com>
2026-07-20 20:56:58 +0200

    Implement Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowings")
+
+    class Meta:
+        ordering = ["-borrow_date"]
+
+    def __str__(self):
+        return f"{self.book.title} - {self.user.email}"
