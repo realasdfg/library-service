4ca6bb7cc4947991fd026bb0c20ea1b376de73c1
Roman Popadiuk <realasdfga@gmail.com>
2026-07-21 02:24:00 +0200

    Add user_id field to BorrowingFilter

--- borrowings/filters.py
+++ borrowings/filters.py
@@ -7,7 +7,8 @@ class BorrowingFilter(django_filters.FilterSet):
     is_active = django_filters.BooleanFilter(
         field_name="actual_return_date", lookup_expr="isnull"
     )
+    user_id = django_filters.NumberFilter(field_name="user", lookup_expr="id")
 
     class Meta:
         model = Borrowing
-        fields = ("is_active",)
+        fields = ("is_active", "user_id")
